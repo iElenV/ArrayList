f75432176a0b8c34a139a945fb5fcdff92b4cb4e
iElenV <energyzer_85@mail.ru>
2023-12-07 21:21:24 +0300

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 
 public class Main {
-    public static void main(String[] args)
-    {
-//ArrayList <Integer> arrayList = new ArrayList <> ();
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4};
+        ArrayList arr = new ArrayList(12);
+        arr.Add(1);
+        arr.Add(1);
+        arr.Add(1);
+        for (int i = 0; i < arr.Count; i++) {
+            System.out.println(arr.Get(i));
 
-
-
-
+        }
     }
 }
