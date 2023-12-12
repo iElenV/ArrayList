add1c95f8dfb69c96d9322fbbf28407a0435b1ba
iElenV <energyzer_85@mail.ru>
2023-12-12 20:15:19 +0300

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         arr.Add(1);
         arr.Add(1);
         arr.Add(new int[]{1, 2,3});
-        System.out.println( "Длина массива " + arr.Count);
+        //System.out.println( "Длина массива " + arr.Count);
         for (int i = 0; i < arr.Count; i++) {
             System.out.println(arr.Get(i));
 
