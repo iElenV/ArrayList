8f9fdd94fbaa27757225cf44817c2c49897ceeff
iElenV <energyzer_85@mail.ru>
2023-12-12 20:37:24 +0300

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,17 @@ public class Main {
         arr.Add(1);
         arr.Add(1);
         arr.Add(new int[]{1, 2,3});
+        PrintArr (arr);
         //System.out.println( "Длина массива " + arr.Count);
-        for (int i = 0; i < arr.Count; i++) {
-            System.out.println(arr.Get(i));
+//        for (int i = 0; i < arr.Count; i++) {
+//            System.out.println(arr.Get(i));
+//
+//        }
+    }
+
+    public static void PrintArr (ArrayList array) {
+        for (int i = 0; i < array.Count; i++) {
+            System.out.println(array.Get(i));
 
         }
     }
